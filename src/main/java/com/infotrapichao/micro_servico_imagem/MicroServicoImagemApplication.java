6dd4cd6ee1faba1e581f8f5a838c65abc91ffe05
MatheusHard <matheus@novetech.com.br>
2026-06-10 13:26:56 -0300

    first commit

---
+++ src/main/java/com/infotrapichao/micro_servico_imagem/MicroServicoImagemApplication.java
@@ -0,0 +1,13 @@
+package com.infotrapichao.micro_servico_imagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicoImagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicoImagemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infotrapichao/micro_servico_imagem/MicroServicoImagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infotrapichao.micro_servico_imagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicoImagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
